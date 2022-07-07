5b740cf9913456b462d664775f1b872a06447304
cybot1711 <jahil.khalfe@gmail.com>
2022-07-07 19:49:09 +0200

    Start styling the profile

--- src/bundles/Repo/Repo.tsx
+++ src/bundles/Repo/Repo.tsx
@@ -1,7 +1,42 @@
 import { useParams } from 'react-router-dom'
 import React from 'react'
+import {
+  StyledContainer,
+  StyledContent,
+  StyledDescription,
+  StyledDivider,
+  StyledImage,
+  StyledImageContainer,
+  StyledImageFrame,
+} from './styles'
 
 export const Repo = () => {
   const params = useParams()
-  return <h1 style={{ color: 'white' }}>repos: {params.slug}</h1>
+  return (
+    <StyledContainer>
+      <StyledContent>
+        <h1
+          style={{
+            color: 'white',
+            letterSpacing: '-0.1rem',
+            width: '100%',
+            fontSize: '3rem',
+            lineHeight: '1.125',
+            fontWeight: 'bold',
+          }}
+        >
+          GoDaddy Repos
+        </h1>
+        <StyledImageContainer>
+          <StyledImageFrame>
+            <StyledImage src='https://place-hold.it/166x166' />
+          </StyledImageFrame>
+        </StyledImageContainer>
+
+        <StyledDescription>Description goes here</StyledDescription>
+
+        <StyledDivider />
+      </StyledContent>
+    </StyledContainer>
+  )
 }
---
+++ src/bundles/Repo/styles.ts
@@ -0,0 +1,63 @@
+import { styled } from '@mui/material/styles'
+import { Container } from '@mui/material'
+
+export const StyledContainer = styled(Container)(({ theme }) => ({
+  marginLeft: 'unset',
+  height: '100vh',
+  display: 'flex',
+  flex: 1,
+  flexDirection: 'column',
+  backdropFilter: 'blur(8px)',
+  boxShadow: '0px 10px 15px 10px rgb(0 0 0 / 15%)',
+  backgroundColor: 'rgb(228 228 228 / 15%)',
+  [theme.breakpoints.up('md')]: {
+    maxWidth: '50%',
+  },
+}))
+
+export const StyledContent = styled('div')(({ theme }) => ({
+  padding: theme.spacing(7),
+}))
+
+export const StyledImageContainer = styled('div')({
+  display: 'block',
+  lineHeight: 0,
+  maxWidth: '100%',
+  position: 'relative',
+})
+
+export const StyledImageFrame = styled('span')({
+  width: '7rem',
+  borderRadius: '100%',
+  borderColor: 'rgba(255,255,255,0.149)',
+  borderStyle: 'solid',
+  borderWidth: 1,
+  backfaceVisibility: 'hidden',
+  transform: 'translate3d(0, 0, 0)',
+  display: 'inline-block',
+  maxWidth: '100%',
+  overflow: 'hidden',
+  verticalAlign: 'top',
+  padding: '0.5rem',
+})
+
+export const StyledImage = styled('img')({
+  borderRadius: 'inherit',
+  maxWidth: '100%',
+  verticalAlign: 'top',
+  width: 'inherit',
+})
+
+export const StyledDescription = styled('p')({
+  color: '#FFB4A3',
+  letterSpacing: '-0.025rem',
+  width: '100%',
+  fontSize: '1em',
+  lineHeight: 1.75,
+  fontWeight: 400,
+})
+
+export const StyledDivider = styled('hr')({
+  color: '#FFB4A3',
+  marginBottom: '3rem',
+})
